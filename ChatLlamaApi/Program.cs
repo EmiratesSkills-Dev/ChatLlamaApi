81cd2628e1621ffcaa46ca63950724b9ce45163d
MARCOS SOARES <marcos.soares@emiratesskills.ae>
2025-07-07 16:30:42 +0400

    Liberacao de cors

--- ChatLlamaApi/Program.cs
+++ ChatLlamaApi/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 app.UseAuthorization();
 
